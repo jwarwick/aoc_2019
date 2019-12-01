d785318c1d11e0f92ad432b33f01432783c41890
John Warwick <jwarwick@gmail.com>
2019-12-01 11:36:09 -0500

    Added typespecs

--- apps/util/lib/util.ex
+++ apps/util/lib/util.ex
@@ -6,6 +6,7 @@ defmodule Util do
   @doc """
   Generate path string for a file in the /priv directory
   """
+  @spec priv_file(atom(), String.t()) :: String.t()
   def priv_file(app, file) do
     Application.app_dir(app, "priv") |> Path.join(file)
   end
@@ -13,6 +14,7 @@ defmodule Util do
   @doc """
   Read a file into a list of strings
   """
+  @spec split_file(Path.t()) :: [String.t()]
   def split_file(file) do
     {:ok, contents} = File.read(file)
     String.split(contents, "\n", trim: true)
@@ -21,6 +23,7 @@ defmodule Util do
   @doc """
   Read a file into a list of integers
   """
+  @spec split_file_to_ints(Path.t()) :: [integer()]
   def split_file_to_ints(file) do
     split_file(file)
     |> Enum.map(&String.to_integer/1)
