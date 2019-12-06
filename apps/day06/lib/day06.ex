30905147cbe70769d2253f0d9ae9bc98ba572c26
John Warwick <jwarwick@gmail.com>
2019-12-06 08:19:25 -0500

    Day 6, Part 2

--- apps/day06/lib/day06.ex
+++ apps/day06/lib/day06.ex
@@ -11,6 +11,14 @@ defmodule Day06 do
     |> orbit_checksum()
   end
 
+  @doc """
+  Count transfers to get to Santa
+  """
+  def part2 do
+    load_graph()
+    |> orbit_transfer_count("YOU", "SAN")
+  end
+
   defp load_graph do
     Util.priv_file(:day06, "day6_input.txt")
     |> File.read!()
@@ -22,25 +30,36 @@ defmodule Day06 do
   """
   def parse_graph(s) do
     String.split(s, "\n", trim: true)
-    |> Enum.map(&(String.split(&1, ")", trim: true)))
+    |> Enum.map(&String.split(&1, ")", trim: true))
     |> Enum.reduce(Graph.new(), &add_edge/2)
   end
 
   defp add_edge([a, b], graph) do
     Graph.add_edge(graph, b, a)
+    |> Graph.add_edge(a, b)
   end
 
   @doc """
   Compute the Orbit Count Checksum
   """
   def orbit_checksum(g) do
-    Enum.reduce(Graph.vertices(g), 0, fn (v, acc) -> acc + depth(g, v) end)
+    Enum.reduce(Graph.vertices(g), 0, fn v, acc -> acc + depth(g, v) end)
   end
 
   defp depth(_g, "COM"), do: 0
+
   defp depth(g, v) do
     Graph.get_shortest_path(g, v, "COM")
     |> Enum.count()
     |> Kernel.-(1)
+  end
+
+  @doc """
+  Count the number of orbital transfers required to move between targets
+  """
+  def orbit_transfer_count(g, a, b) do
+    Graph.get_shortest_path(g, a, b)
+    |> Enum.count()
+    |> Kernel.-(3)
   end
 end
--- apps/day06/test/day06_test.exs
+++ apps/day06/test/day06_test.exs
@@ -19,12 +19,37 @@ defmodule Day06Test do
   test "read orbit map" do
     g = Day06.parse_graph(@test_map)
     assert 12 == Enum.count(Graph.vertices(g))
-    assert 11 == Enum.count(Graph.edges(g))
-    assert Graph.is_tree?(g)
+    assert 11 == Enum.count(Graph.edges(g)) / 2
+    # assert Graph.is_tree?(g)
   end
 
   test "orbit checksum" do
     g = Day06.parse_graph(@test_map)
     assert 42 == Day06.orbit_checksum(g)
+  end
+
+  test "part1" do
+    assert 295_936 == Day06.part1()
+  end
+
+  @part2_test_map """
+  COM)B
+  B)C
+  C)D
+  D)E
+  E)F
+  B)G
+  G)H
+  D)I
+  E)J
+  J)K
+  K)L
+  K)YOU
+  I)SAN
+  """
+
+  test "orbit_count" do
+    g = Day06.parse_graph(@part2_test_map)
+    assert 4 == Day06.orbit_transfer_count(g, "YOU", "SAN")
   end
 end
