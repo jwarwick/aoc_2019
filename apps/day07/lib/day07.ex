e7b72cedf486fe6fbddac7074969671e78445fdb
John Warwick <jwarwick@gmail.com>
2019-12-07 13:35:16 -0500

    Day 7, Part 2

--- apps/day07/lib/day07.ex
+++ apps/day07/lib/day07.ex
@@ -13,38 +13,97 @@ defmodule Day07 do
   end
 
   @doc """
+  Find largest output signal that can be sent to thrusters in feedback mode
+  """
+  def part2 do
+    Util.priv_file(:day07, "day7_input.txt")
+    |> Intcode.load()
+    |> max_feedback_thrust(5..9)
+  end
+
+  @doc """
   Find the max thrust
   """
   def max_thrust(prog, phases) do
+    max_for_type(prog, phases, &compute_thrust/2)
+  end
+
+  @doc """
+  Find the max thrust in the feedback loop configuration
+  """
+  def max_feedback_thrust(prog, phases) do
+    max_for_type(prog, phases, &compute_feedback_thrust/2)
+  end
+
+  defp max_for_type(prog, phases, f) do
     Enum.into(phases, [])
     |> permutations()
-    |> Enum.map(&(compute_thrust(prog, &1)))
+    |> Enum.map(&f.(prog, &1))
     |> Enum.max()
   end
 
   @doc """
   Compute the output signal sent to the thrusters for a given phase setting
   """
-  def compute_thrust(prog, [first | rest]) do
-    rev_phase = Enum.reverse(rest)
-    b_pid = Enum.reduce(rev_phase, self(),
-                                        fn (p, next_pid) -> 
-                                          Process.spawn(Intcode, :run,
-                                                        [prog, [p],
-                                                         fn () -> receive do x->x end end,
-                                                         &(send(next_pid, &1))],
-                                                         [])
-                                        end)
-    Process.spawn(Intcode, :run,
-                  [prog, [first, 0],
-                   nil,
-                   &(send(b_pid, &1))],
-                   [])
+  def compute_thrust(prog, phases) do
+    [a_pid | _pids] = spawn_amps(prog, phases)
+    send(a_pid, 0)
+
     receive do
       x -> x
     end
   end
 
+  @doc """
+  Compute the output signal sent to the thrusters in feedback mode
+  """
+  def compute_feedback_thrust(prog, phases) do
+    [a_pid | _pids] = spawn_amps(prog, phases)
+    send(a_pid, 0)
+    await(a_pid)
+  end
+
+  defp await(pid) do
+    receive do
+      x ->
+        if Process.alive?(pid) do
+          send(pid, x)
+          await(pid)
+        else
+          x
+        end
+    end
+  end
+
+  defp spawn_amps(prog, phases) do
+    Enum.reverse(phases)
+    |> Enum.reduce(
+      [self()],
+      fn p, others = [next_pid | _rest] ->
+        pid =
+          Process.spawn(
+            Intcode,
+            :run,
+            [
+              prog,
+              [p],
+              fn ->
+                receive do
+                  x -> x
+                end
+              end,
+              &send(next_pid, &1)
+            ],
+            []
+          )
+
+        [pid | others]
+      end
+    )
+  end
+
   defp permutations([]), do: [[]]
-  defp permutations(list), do: for elem <- list, rest <- permutations(list--[elem]), do: [elem|rest]
+
+  defp permutations(list),
+    do: for(elem <- list, rest <- permutations(list -- [elem]), do: [elem | rest])
 end
